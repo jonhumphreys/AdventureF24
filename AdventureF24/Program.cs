bc01d0929bd7b788cc7ba68685a78ee0afc76007
jonhumphreys <jonhumphreys@chapman.edu>
2024-09-05 18:43:31 -0700

    Added user input and game loop

--- AdventureF24/Program.cs
+++ AdventureF24/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AdventureF24;
+
+public class Program
+{
+     public static void Main(string[] args)
+     {
+         Game.Play();
+     }
+}
