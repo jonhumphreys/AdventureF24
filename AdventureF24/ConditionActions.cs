76f61049fb61f01d3050cb7814ffb32abe202f36
jonhumphreys <jonhumphreys@chapman.edu>
2024-11-07 18:45:21 -0800

    After 11/7 class

---
+++ AdventureF24/ConditionActions.cs
@@ -0,0 +1,31 @@
+namespace AdventureF24;
+
+public static class ConditionActions
+{
+    public static Action WriteOutput(string message)
+    {
+        return () => IO.WriteLine(message);
+    }
+    
+    // add a map connection
+    
+    // remove a map connection
+    
+    // move player to location
+    
+    // if player dies
+    
+    // add item to inventory
+    
+    // add item to room
+    
+    // remove item from inventory
+    
+    // remove item from room
+    
+    // spawn mob
+    
+    // remove mob
+    
+    // change state
+}
